7a67f38e3aa5f25aa1f597eb96d2c0ed3d3609e6
Clann_Home:김홍제_집 <restoreclann@gmail.com>
2022-03-09 20:04:12 +0900

    vue todo with typescript

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,102 @@
 <template>
   <div>
-    <h1>Vue Todo with Typescript</h1>
+    <header>
+      <h1>Vue Todo with Typescript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <div>
+        <ul>
+          <TodoListItem
+            v-for="(todoItem, idx) in todoItems"
+            :key="idx"
+            :index="idx"
+            :todoItem="todoItem"
+            @toggle="toggleTodoItemComplete"
+            @remove="removeTodoItem"
+          />
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoListItem from "./components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: Todo[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch(): Todo[] {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    const result = JSON.parse(todoItems);
+    return result;
+  },
+};
+
+export interface Todo {
+  title: string;
+  done: boolean;
+}
 
 export default Vue.extend({
+  components: { TodoInput, TodoListItem },
   data() {
     return {
-      name: "hi",
+      todoText: "",
+      todoItems: [] as Todo[],
     };
+  },
+  methods: {
+    updateTodoText(value: string) {
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      const todo: Todo = {
+        title: value,
+        done: false,
+      };
+
+      this.todoItems.push(todo);
+      storage.save(this.todoItems);
+      // localStorage.setItem(value, value);
+      this.initTodoTest();
+    },
+    initTodoTest() {
+      this.todoText = "";
+    },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch().sort((a, b) => {
+        if (a.title < b.title) return -1;
+        else if (a.title > b.title) return 1;
+        else return 0;
+      });
+    },
+    toggleTodoItemComplete(todoItem: Todo, index: number) {
+      this.todoItems.splice(index, 1, {
+        ...todoItem,
+        done: !todoItem.done,
+      });
+      storage.save(this.todoItems);
+    },
+    removeTodoItem(index: number) {
+      console.log("remove", index);
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
+    },
+  },
+  created() {
+    this.fetchTodoItems();
   },
 });
 </script>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input
+      id="todo-input"
+      type="text"
+      :value="item"
+      @input="handleInput"
+      @keyup.enter="addTodo"
+    />
+    <button @click="addTodo" type="button">추가</button>
+  </div>
+</template>
+
+<script lang="ts">
+/* eslint-disable */
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    item: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+  methods: {
+    handleInput(event: InputEvent) {
+      // console.log(event);
+      // if(!event.target) return ;
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
+    },
+    addTodo() {
+      this.$emit("add");
+    },
+  },
+});
+</script>
+
+<style scoped></style>
